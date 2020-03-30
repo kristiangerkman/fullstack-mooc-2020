d3b81f772ec426547ee42ac71c56d06d0f422d17
Kristedd <kristiangerkman1@gmail.com>
2020-03-31 00:17:28 +0300

    anecdotes done

--- pt1/anecdotes/src/App.js
+++ pt1/anecdotes/src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = props => {
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState([]);
+
+  useEffect(() => {
+    const tmp = new Array(props.anecdotes.length);
+    for (let i = 0; i < tmp.length; i++) {
+      tmp[i] = 0;
+    }
+    setPoints(tmp);
+    // eslint-disable-next-line
+  }, []);
+
+  const ReturnMostVoted = points => {
+    let max = points[0];
+    let maxIndex = 0;
+
+    for (let i = 1; i < points.length; i++) {
+      if (points[i] > max) {
+        maxIndex = i;
+        max = points[i];
+      }
+    }
+
+    return maxIndex;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Anecdote of the day</h1>
+      {props.anecdotes[selected]}
+      <br />
+      <p>has {points[selected]} points</p>
+
+      <button
+        onClick={() => {
+          while (1) {
+            let newNum = Math.floor(
+              Math.random() * Math.floor(props.anecdotes.length)
+            );
+            console.log("asd" + newNum);
+            if (newNum !== selected) {
+              setSelected(newNum);
+              break;
+            }
+          }
+        }}
+      >
+        Next anecdote
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...points];
+          copy[selected] += 1;
+          setPoints(copy);
+        }}
+      >
+        vote
+      </button>
+      <h1>Anecdote with most votes</h1>
+
+      {props.anecdotes[ReturnMostVoted(points)]}
+      <br />
+      <p>has {points[ReturnMostVoted(points)]} points</p>
     </div>
   );
-}
+};
 
 export default App;
