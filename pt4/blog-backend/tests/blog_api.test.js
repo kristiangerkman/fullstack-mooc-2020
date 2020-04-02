e144478884c059fe8b4e251fc321ac97f3a028e3
Kristedd <kristiangerkman1@gmail.com>
2020-04-03 01:38:10 +0300

    nfaoisc

--- pt4/blog-backend/tests/blog_api.test.js
+++ pt4/blog-backend/tests/blog_api.test.js
@@ -1,14 +1,38 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-
 const api = supertest(app);
 
+const Blog = require("../models/blog");
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  console.log("cleared");
+  const newBlog = new Blog({
+    author: "a",
+    title: "asd",
+    url: "dsdsd",
+    likes: 0
+  });
+  await newBlog.save();
+});
+
 test("blogs are returned as json", async () => {
-  await api
+  const res = await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+
+  expect(res.body.length).toEqual(1);
+});
+
+test("blogs are returned as json", async () => {
+  const res = await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  expect(res.body.length).toEqual(1);
 });
 
 afterAll(() => {
