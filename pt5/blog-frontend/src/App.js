2b14a9aee57a64eb49e3eb1bd4160d55c85111ae
Kristedd <kristiangerkman1@gmail.com>
2020-04-03 20:06:51 +0300

    asd

--- pt5/blog-frontend/src/App.js
+++ pt5/blog-frontend/src/App.js
@@ -3,7 +3,21 @@ import LoginForm from "./components/LoginForm";
 import NewBlogForm from "./components/NewBlogForm";
 
 const App = () => {
+  const [newUser, setNewUser] = useState({
+    username: "",
+    name: "",
+    password: ""
+  });
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+  const [user, setUser] = useState({ username: "", name: "", token: "" });
   const [loggedIn, setLoggedIn] = useState(false);
+
+  const [allBlogs, setAllBlogs] = useState([]);
+  const [newBlog, setNewBlog] = useState({ title: "", author: "", url: "" });
+
   if (!loggedIn) {
     return (
       <div>
